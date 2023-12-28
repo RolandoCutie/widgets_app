bc9f70fd3137ca6a8ea8edd40e5dd45ee6b4e403
Rolando Garcia Cutie <rolando.garcia@alivi.com>
2023-12-28 17:41:32 -0500

    creadas todas las pantallas

---
+++ lib/presentation/screens/infinited_scroll/infinited_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfinitedScrollScreen extends StatelessWidget {
+  static const name = 'infinite_screen';
+
+  const InfinitedScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
